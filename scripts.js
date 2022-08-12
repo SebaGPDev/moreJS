a24472816c82e4428aa2fd0f7a8c1ceaf77b9b58
CplusArd <sebakpo120@gmail.com>
2022-08-12 10:14:02 -0300

    repaso js

---
+++ scripts.js
@@ -0,0 +1,41 @@
+//alert("Bienvenido")
+
+//EJERCISIOS
+
+//1) Pedir al usuario que ingrese su nombre. Guardar ese nombre en una variable y utilizarlo para saludar al usuario.
+
+//let nombre = prompt("Cual es tu nombre?")
+//alert("Bienvenido" + " "+ nombre)
+
+//2) Pedir al usuario que ingrese 2 numeros y devolver las operaciones
+//Basicas (Suma, Resta, Multiplicacion, Division)
+
+//let numeroDos = parseInt(prompt("Ingrese otro numero"))
+
+// let suma = numeroUno + numeroDos
+// let resta = numeroUno - numeroDos
+// let multiplicacion = numeroUno * numeroDos
+// let division = numeroUno / numeroDos
+
+
+
+//alert("La suma es:" + " "+ suma)
+
+//alert(`La suma es: ${suma}`)
+//alert(`La resta es: ${resta}`)
+//alert(`La multiplicacion es: ${multiplicacion}`)
+//alert(`La division es: ${division}`)
+
+//document.write(`
+//<p>la suma es: ${suma} </p><br>
+//<p>la resta es: ${resta} </p><br>
+//<p>la multiplicacion es: ${multiplicacion}</p><br>
+//<p>la division es: ${division}</p><br>
+//`)
+
+//console.log(typeof(+sting))
+
+//3) Pedir al usuario que ingrese su apellido y devolver en mayuscula
+
+//let apellido = prompt("Ingrese su apellido en minuscula")
+//alert(apellido.toUpperCase())
