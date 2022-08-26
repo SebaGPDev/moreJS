f6bb631c1fb6ebede231c83d05f2ee559135667f
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-08-26 10:23:47 -0300

    Practica de funciones 26-08-2022

---
+++ Practica/1.js
@@ -0,0 +1,30 @@
+// crear un programa que pida a un usuario que ingrese un numero y devuelva un mensaje diciendo si es positivo, negativo o 0
+
+/* function ingresoNumero(unNumero) {
+    let otroNumero = parseFloat(prompt(unNumero));
+    if (otroNumero > 0) {
+        return document.write("El numero que ingreso es positivo");
+    } else if (otroNumero < 0) {
+        return document.write("El numero que ingreso es negativo");
+    } else if (otroNumero === 0) {
+        return document.write("El numero que ingreso es 0")
+    } else {
+        return document.write("No ingreso un numero valido");
+    }
+}
+ingresoNumero(); */
+
+/* const ingresoNumero = unNumero => {
+    let otroNumero = parseFloat(prompt(unNumero));
+    if (otroNumero > 0) {
+        return document.write("El numero que ingreso es positivo");
+    } else if (otroNumero < 0) {
+        return document.write("El numero que ingreso es negativo");
+    } else if (otroNumero === 0) {
+        return document.write("El numero que ingreso es 0")
+    } else {
+        return document.write("No ingreso un numero valido");
+    }
+}
+ingresoNumero(); */
+
---
+++ Practica/2.js
@@ -0,0 +1,34 @@
+// Crear un programa que pida al usuario un numero y que devuelva un mensaje diciendo si el numero es par
+
+/* function ingresoNumero(unNumero) {
+  let otroNumero = parseInt(prompt(unNumero));
+  if (otroNumero % 2 === 0) {
+    return document.write("Si el numero es par");
+  } else {
+    return document.write("El numero no es par o no es un numero");
+  }
+}
+ingresoNumero(); */
+
+/* const ingresoNumero = (unNumero) => {
+    let otroNumero = parseInt(prompt(unNumero));
+    if (otroNumero % 2 === 0) {
+      return document.write("Si el numero es par");
+    } else {
+      return document.write("El numero no es par o no es un numero");
+    }
+}
+
+ingresoNumero(); */
+
+/* const primo = (unNumero) => {
+    let otroNumero = parseInt(prompt(unNumero));
+    if(((otroNumero/otroNumero) ===1 || (otroNumero/1) === otroNumero) || (otroNumero % otroNumero != 1 && otroNumero % 1 != otroNumero)) {
+        document.write(`El numero es primo`)
+   } else {
+    document.write(`El numero no es primo`)
+   }
+}
+
+primo()
+ */
