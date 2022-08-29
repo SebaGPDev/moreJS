348925d673957f8fac1f374c3d9488d6a3b11311
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-08-29 16:46:06 -0300

    01 terminado 29-08-2022

--- TLPT2/01-funciones-promesas.js
+++ TLPT2/01-funciones-promesas.js
@@ -1,11 +1,45 @@
-    const promesa = new Promise((resolve, reject)=>{
+//Primer Promesa
+const promesa = new Promise((resolve, reject) => {
+  10 === 11
+    ? resolve("Los valores son iguales")
+    : reject("Los valores son distintos");
+});
 
-        if( 10 === 11){
-            resolve('Los valores son iguales');
-        } else {
-            reject('Los valores son distintos')
-        }
+promesa
+  .then((correcto) => console.log(correcto))
+  .catch((error) => console.error(error));
 
-    });
+//Segunda Promesa
+// Promesa dentro en una función.
+const promesaDos = (provincia) => {
+  const promesa = new Promise((resolve, reject) => {
+    "Formosa" === provincia
+      ? resolve("Usted está ubicado en Formosa")
+      : reject("Error al realizar la consulta");
+  });
+  promesa
+    .then((correcto) => console.log(correcto))
+    .catch((error) => console.error(error));
+};
+promesaDos("Formosa");
 
-    promesa
+//Tercera Promesa
+const promesaTres = async (valorBooleano) => {
+  return true === valorBooleano
+    ? "Los valores son iguales"
+    : "Los valores son distintos";
+};
+
+promesaTres(true)
+  .then((correcto) => console.log(correcto))
+  .catch((error) => console.error(error));
+
+const promesaCuatro = async (arguments) => {
+  return true === arguments
+    ? "Los valores son iguales"
+    : "Los valores son distintos";
+};
+
+promesaCuatro(true)
+  .then((correcto) => console.log(correcto))
+  .catch((error) => console.error(error));
